8fc7318a08c8fa6ab81eb523975eb44e3ae5d21d
Yigit Sayar <yigitsayar@yandex.com>
2024-08-16 18:46:27 +0300

    SDL-OpenGL init

--- main.cpp
+++ main.cpp
@@ -1,14 +1,124 @@
+//  g++ main.cpp ./src/glad.c -I./include/ -o prog -lSDL2 -ldl
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
 
-#include <SDL.h>
-// g++ main.cpp -o prog -I/usr/include/SDL2 `pkg-config --libs sdl2`
+// Globals
+int gScreenWidth = 640;
+int gScreenHeight = 480;
+SDL_Window* gGraphicsApplicationWindow = nullptr;
+SDL_GLContext gOpenGLContext = nullptr;
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cout << "SDL could not be initialized " << SDL_GetError();
-    } else {
-        std::cout << "SDL video system is ready to go \n";
+bool gQuit = false; // If true, we quit
+
+void GetOpenGLVersionInfo()
+{
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Shading Language Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+}
+
+void InitializeProgram()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL2 could not be initialized video subsystem" << std::endl;
+        exit(1);
     }
+
+    // Setting OpenGL version 4.1, it should work on Windows, Linux, and Mac, but beyond 4.1 Mac does not support OpenGL
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    // Setting OpenGl core profile, so obsolite functions of OpenGL are disabled (this setting is advised)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+
+    gGraphicsApplicationWindow = SDL_CreateWindow("OpenGL Window",
+                              0, 0, gScreenWidth,
+                              gScreenWidth, SDL_WINDOW_OPENGL);
+
+    if (gGraphicsApplicationWindow==nullptr)
+    {
+        std::cout << "SDL_Window was not able to be created" << std::endl;
+        exit(1);
+    }
+
+    gOpenGLContext = SDL_GL_CreateContext(gGraphicsApplicationWindow);
+
+    if (gOpenGLContext==nullptr)
+    {
+        std::cout << "OpenGl context not avaliable" << std::endl;
+        exit(1);
+    }
+
+    // intialize the Glad library
+    if(!gladLoadGLLoader(SDL_GL_GetProcAddress))
+    {
+        std::cout << "glad was not initialized" << std::endl;
+        exit(1);
+    }
+}
+
+void Input()
+{
+    SDL_Event e;
+
+    while (SDL_PollEvent(&e) != 0)
+    {
+        if (e.type == SDL_QUIT)
+        {
+            std::cout << "Goodbye!" << std::endl;
+            gQuit = true;
+        }
+    }
+}
+
+void PreDraw()
+{
+
+}
+
+void Draw()
+{
+
+}
+
+void MainLoop()
+{
+    while (!gQuit)
+    {
+        Input();
+
+        PreDraw();
+
+        Draw();
+
+        // Update the screen
+        SDL_GL_SwapWindow(gGraphicsApplicationWindow);
+    }
+}
+
+void CleanUp()
+{
+    SDL_DestroyWindow(gGraphicsApplicationWindow);
+
+    SDL_Quit();
+}
+
+int main() 
+{
+    
+    InitializeProgram();
+
+    GetOpenGLVersionInfo();
+
+    MainLoop();
+
+    CleanUp();
 
     return 0;
 }
